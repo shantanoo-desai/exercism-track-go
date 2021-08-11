0d716b462ebdb9893cbd8068f546181bb037fc32
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-08-11 16:06:50 +0200

    feat: add scrabble-score exercise
    Signed-off-by: Shantanoo Desai <shantanoo.desai@gmail.com>

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,37 @@
+package scrabble
+
+import "strings"
+
+// Score: Function to calculate Scrabble-Score
+// for a given Word
+// Score Sheet:
+//Letter                           Value
+// A, E, I, O, U, L, N, R, S, T       1
+// D, G                               2
+// B, C, M, P                         3
+// F, H, V, W, Y                      4
+// K                                  5
+// J, X                               8
+// Q, Z                               10
+func Score(word string) int {
+	score := 0
+	for _, letter := range strings.ToUpper(word) {
+		switch letter {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score += 1
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
+	}
+	return score
+}
